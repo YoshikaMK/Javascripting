3ad51862736a227f0162a434ce48e9b59c0d2516
Yoshika <Yoshikakeating@gmail.com>
2018-09-21 22:06:30 +0000

    Initial commit with all my work so far

---
+++ looping-through-arrays.js
@@ -0,0 +1,5 @@
+pets = ['cat', 'dog', 'rat'];
+
+for(var i = 0; i < pets; pets[i] = pets[i] + 's'; )
+
+console.log(pets);
---
+++ rounding-numbers.js
@@ -0,0 +1,4 @@
+var roundUp = 1.5 ;
+Math.round(1.5);
+var rounded = roundUp ;
+console.log(Math.round(1.5));
